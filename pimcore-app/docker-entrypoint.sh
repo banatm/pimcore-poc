a79f82c3540a490cbdaf275680d4c7c1a117a4e8
banatm <michal.banat@synevo.pl>
2019-10-30 10:31:43 +0100

    utilize datadefinitions

--- pimcore-app/docker-entrypoint.sh
+++ pimcore-app/docker-entrypoint.sh
@@ -6,12 +6,23 @@ if [ -e /var/www/html/var/config/system.yml ]; then
 else
   cd /var/www/html
   ./vendor/bin/pimcore-install --no-interaction
+  #install bundles
   ./bin/console pimcore:bundle:enable DataDefinitionsBundle
   ./bin/console pimcore:bundle:install DataDefinitionsBundle
   ./bin/console pimcore:bundle:enable  PimcoreDataHubBundle
   ./bin/console pimcore:bundle:install  PimcoreDataHubBundle
+  #install our bundle
   ./bin/console pimcore:bundle:enable ProductsBundle
   ./bin/console pimcore:bundle:install ProductsBundle
+  #import definitions
+  ./bin/console data-definitions:definition:import:import ./src/ProductsBundle/Resources/import/import-definition-synevo-ro-main-categories.json
+  ./bin/console data-definitions:definition:import:import ./src/ProductsBundle/Resources/import/import-definition-synevo-ro-subcategories.json
+  ./bin/console data-definitions:definition:import:import ./src/ProductsBundle/Resources/import/import-definition-synevo-ro-products.json
+  #load data
+  ./bin/console data-definitions:import -d synevo-ro-main-categories
+  ./bin/console data-definitions:import -d synevo-ro-subcategories
+  ./bin/console data-definitions:import -d synevo-ro-products
+
   chown -R www-data:www-data /var/www/html
 fi
 
